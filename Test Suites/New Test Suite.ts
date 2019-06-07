f9ea9580d6832642aeff7297c077e8cd2cec64bd
SSaxena <SSaxena@CUA44495VQA027.jetblue.com>
2019-06-07 17:01:51 +0530

    Test suite has been executed

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>54faf7a9-fc29-46f2-992c-d332bda5b5bd</testSuiteGuid>
+   <testCaseLink>
+      <guid>0732ef7a-8161-41df-9b64-7bb51b5c0afc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Enroll CLM</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
